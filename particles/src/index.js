36c318d105ab829bc28d1bfdf87ad978e393bf88
deguchi <deguchi37458@gmail.com>
2024-03-21 15:58:08 +0900

    [update] particles

--- particles/src/index.js
+++ particles/src/index.js
@@ -2,80 +2,101 @@ import vertexShader from "./shader/vertexShader";
 import fragmentShader from "./shader/fragmentShader";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "lil-gui";
 
-// Sizes
+// デバッグ(色つけるときに追加)
+const gui = new dat.GUI()
+
+// 必須の3要素
+// Canvas
+const canvas = document.querySelector(".webgl");
+
+//Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-let rot = 0; // 角度
 
-// レンダラーを作成
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector(".webgl"),
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(sizes.width, sizes.height);
-
-// シーンを作成
+// Scene
 const scene = new THREE.Scene();
 
-// カメラを作成
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 6;
+scene.add(camera);
 
-// 星屑を作成します (カメラの動きをわかりやすくするため)
-createStarField();
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-function createStarField() {
-  // 頂点情報を格納する配列
-  const vertices = [];
+// Donuts
+const donutsParticlesGeometry = new THREE.TorusGeometry(2.8, 0.7, 16, 100);
+const donutsParticlesMaterial = new THREE.PointsMaterial({
+  size: 0.023,
+  color: 'red'
+});
+const donutsParticlesMesh = new THREE.Points(donutsParticlesGeometry, donutsParticlesMaterial);
+scene.add(donutsParticlesMesh)
 
-  // 配置する範囲
-  const SIZE = 3000;
-  // 配置する個数
-  const LENGTH = 1000;
+gui.addColor(donutsParticlesMaterial, 'color');
 
-  for (let i = 0; i < LENGTH; i++) {
-    const x = SIZE * (Math.random() - 0.5);
-    const y = SIZE * (Math.random() - 0.5);
-    const z = SIZE * (Math.random() - 0.5);
-
-    vertices.push(x, y, z);
-  }
-
-  // 形状データを作成
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
-
-  // マテリアルを作成
-  const material = new THREE.PointsMaterial({
-    // 一つ一つのサイズ
-    size: 10,
-    // 色
-    color: 0xffffff,
-  });
-
-  // 物体を作成
-  const mesh = new THREE.Points(geometry, material);
-  scene.add(mesh);
+// Bg Particle
+const count = 5000;
+const particlesGeometry = new THREE.BufferGeometry();
+const positionArray = new Float32Array(count * 3);
+for(let i = 0; i < count * 3; i++){
+  positionArray[i] = (Math.random() - 0.5 ) * 15;
 }
+
+particlesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positionArray, 3)
+)
+const ParticlesMaterial = new THREE.PointsMaterial({
+  size: 0.01,
+  color :"#ffffff"
+})
+const particlesMesh = new THREE.Points(particlesGeometry, ParticlesMaterial)
+scene.add(particlesMesh)
+
+//カメラ制御
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+
+// アニメーション
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime(); // 経過時間を取得
+
+  camera.position.x = Math.cos(elapsedTime * 0.5) * 6;
+  camera.position.z = Math.sin(elapsedTime * 0.5) * 6;
+
+  controls.update();
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
 
 tick();
 
-// 毎フレーム時に実行されるループイベントです
-function tick() {
-  // rot += 1;
+//ブラウザのリサイズ操作
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-  // ラジアンに変換する
-  const radian = (rot * Math.PI) / 180;
-  // 角度に応じてカメラの位置を設定
-  camera.position.x = 1000 * Math.sin(radian);
-  camera.position.z = 1000 * Math.cos(radian);
-  // 原点方向を見つめる
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-  // レンダリング
-  renderer.render(scene, camera);
-
-  requestAnimationFrame(tick);
-}
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
